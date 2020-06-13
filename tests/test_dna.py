734a7a81afea729d2308541e82f327edb4591d24
inbalpreuss <inbalpreuss@gmail.com>
2020-06-13 15:04:06 +0300

    commented out the caching of the .pk

--- tests/test_dna.py
+++ tests/test_dna.py
@@ -25,34 +25,34 @@ def subset_size_and_error_plot(number_of_oligos_per_barcode: int = 20,
     results = {}
     sizes_and_bit_sizes = [(3, 9), (5, 12), (7, 13)]
     res_file = pathlib.Path(f'data/testing/output/error_results_n_oligos_{number_of_oligos_per_barcode}_n_sampled_{number_of_sampled_oligos_from_file}.pk')
-    if not res_file.is_file():
-        for size, bits_per_z in sizes_and_bit_sizes:
-            # products = itertools.product(errors, repeat=3)
-            prods = list(itertools.product(errors, repeat=3))
-            products = [i for i in prods if (i[0] == 0 and i[1] == 0) or (i[0] == 0 and i[2] == 0) or (i[1] == 0 and i[2] == 0)]
-            for prod in products:
-                config = build_config(subset_size=size,
-                                      bits_per_z=bits_per_z,
-                                      letter_replace_error_ratio=prod[0],
-                                      letter_remove_error_ratio=prod[1],
-                                      letter_add_error_ratio=prod[2],
-                                      number_of_oligos_per_barcode=number_of_oligos_per_barcode,
-                                      number_of_sampled_oligos_from_file=number_of_sampled_oligos_from_file)
-                if number_of_sampled_oligos_from_file is None:
-                    n_samples = 'all'
-                else:
-                    n_samples = number_of_sampled_oligos_from_file
-                print(f"[ size: {size:>1} ]",
-                      f"[ errors: (replace: {prod[0]:<6}), (remove: {prod[1]:<6}), (add: {prod[2]:<6}) ]",
-                      f"[ n_oligos_per_barcode: {number_of_oligos_per_barcode:>6} ]",
-                      f"[ m_samples_from_synthesis_file: {n_samples:>6}]")
-                dist, input_data, output_data = run_pipe_with_config(config)
-                pos = size, *prod
-                print(f"[ input data: {input_data} ]\n",
-                      f"[ output data: {output_data}]\n",
-                      f"[ dist: {dist:>5}]\n"
-                      f"======================================================================")
-                results[pos] = {'dist': dist, 'input_data': input_data, 'output_data': output_data}
+    #if not res_file.is_file():
+    for size, bits_per_z in sizes_and_bit_sizes:
+        # products = itertools.product(errors, repeat=3)
+        prods = list(itertools.product(errors, repeat=3))
+        products = [i for i in prods if (i[0] == 0 and i[1] == 0) or (i[0] == 0 and i[2] == 0) or (i[1] == 0 and i[2] == 0)]
+        for prod in products:
+            config = build_config(subset_size=size,
+                                  bits_per_z=bits_per_z,
+                                  letter_replace_error_ratio=prod[0],
+                                  letter_remove_error_ratio=prod[1],
+                                  letter_add_error_ratio=prod[2],
+                                  number_of_oligos_per_barcode=number_of_oligos_per_barcode,
+                                  number_of_sampled_oligos_from_file=number_of_sampled_oligos_from_file)
+            if number_of_sampled_oligos_from_file is None:
+                n_samples = 'all'
+            else:
+                n_samples = number_of_sampled_oligos_from_file
+            print(f"[ size: {size:>1} ]",
+                  f"[ errors: (replace: {prod[0]:<6}), (remove: {prod[1]:<6}), (add: {prod[2]:<6}) ]",
+                  f"[ n_oligos_per_barcode: {number_of_oligos_per_barcode:>6} ]",
+                  f"[ m_samples_from_synthesis_file: {n_samples:>6}]")
+            dist, input_data, output_data = run_pipe_with_config(config)
+            pos = size, *prod
+            print(f"[ input data: {input_data} ]\n",
+                  f"[ output data: {output_data}]\n",
+                  f"[ dist: {dist:>5}]\n"
+                  f"======================================================================")
+            results[pos] = {'dist': dist, 'input_data': input_data, 'output_data': output_data}
 
         with open(res_file, 'wb') as f:
             pickle.dump(results, f)
